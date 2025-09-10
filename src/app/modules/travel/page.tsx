e7b207adb2ac5e48b0a9d2d3d47d5c9ca5c93fb4
Tolbert Omini <ominitolbert@gmail.com>
2025-09-10 04:03:45 +0000

    in NEstTravel and NestStays portal pick the images from pulbic/travel fo

--- src/app/modules/travel/page.tsx
+++ src/app/modules/travel/page.tsx
@@ -145,7 +145,7 @@ const HeroSection = () => {
     return (
         <section className="relative h-[70vh] flex items-center justify-center text-white">
              <Image
-                src="/images/maasaimara.jpg"
+                src="/travel/mara.jpg"
                 alt="Maasai Mara sunrise with an airplane taking off"
                 layout="fill"
                 objectFit="cover"
