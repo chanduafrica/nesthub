ff313ca6d7993aecf0c8dc5b42a60acc42fddd3d
Tolbert Omini <ominitolbert@gmail.com>
2025-09-22 10:29:39 +0000

    errors

--- src/app/vendor-registration/page.tsx
+++ src/app/vendor-registration/page.tsx
@@ -5,7 +5,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Cpu, FileCheck, HomeIcon, Plane, ShoppingCart, Ticket, UserCheck, BedDouble, ArrowRight } from "lucide-react";
+import { FileCheck2, HomeIcon, Plane, ShoppingCart, Ticket, UserCheck, BedDouble, ArrowRight, Cpu } from "lucide-react";
 import Link from "next/link";
 
 const availablePortals = [
