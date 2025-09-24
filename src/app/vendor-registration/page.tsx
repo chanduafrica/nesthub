72c5fadca3bfcfc23a4aa4501a7bc01c3f4c9bb9
Tolbert Omini <ominitolbert@gmail.com>
2025-09-24 01:42:03 +0000

    erros 2

--- src/app/vendor-registration/page.tsx
+++ src/app/vendor-registration/page.tsx
@@ -8,8 +8,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import {
-    FileCheck2, HomeIcon, Plane, ShoppingCart, Ticket, UserCheck, BedDouble, ArrowRight, ArrowLeft, Building, Car, School, Dna,
-    MessageSquare, UtensilsCrossed, Info, FileText, Shield, RefreshCw, Mail as MailIconLucide, Menu
+    FileCheck2, HomeIcon, Plane, ShoppingCart, Ticket, UserCheck, BedDouble, ArrowRight, Building, Car, School, Dna,
+    MessageSquare, UtensilsCrossed, Info, FileText, Shield, RefreshCw, Mail as MailIconLucide, Menu, ArrowLeft
 } from "lucide-react";
 import Link from "next/link";
 import { useToast } from "@/hooks/use-toast";
