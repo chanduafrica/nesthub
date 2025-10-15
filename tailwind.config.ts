02475e950c008e5285f62792370f2aec83d1387b
Tolbert Omini <ominitolbert@gmail.com>
2025-10-15 05:33:30 +0000

    🎨 Brand Colors Element	Color	Use Case Primary	#E65100 (Deep Orange)	But

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,6 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        body: ['Roboto', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
-      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
